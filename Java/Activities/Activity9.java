98af0b88b010a834d845b40e937fa1cfcfbae65c
GaviniNagaMamatha <137032051+GaviniNagaMamatha@users.noreply.github.com>
2025-09-21 21:59:15 +0530

    Update Activity9.java

--- Java/Activities/Activity9.java
+++ Java/Activities/Activity9.java
@@ -1,26 +1,30 @@
 package Activities;
+
 import java.util.ArrayList;
 
 public class Activity9 {
-	public static void main(String[] args) {
-		ArrayList<String> myList = new ArrayList<String>();
-		myList.add("Ane");
-		myList.add("Ani");
-		myList.add("Ano");
-		myList.add("Any");
-		
-		System.out.println("Print All the Objects:");
-		for(String s: myList) {
-			System.out.println(s); 
-			
-		}
-		System.out.println("3rd name in the ArrayList:" + myList.get(2));
-		System.out.println("check if a name exists in the ArrayList:" + myList.contains("Any"));
-		System.out.println("number of names in the ArrayList:" + myList.size());
-		System.out.println("removed name from the list:" + myList.remove("Ano"));
-		System.out.println("number of names in the ArrayList after removal:" + myList.size());
-		
-			
-	}
-
+    public static void main(String[] args) {
+        
+        ArrayList<String> myList = new ArrayList<String>();
+      
+        myList.add("Apple");
+        myList.add("Mango");
+        myList.add("Orange");
+     
+        myList.add(3, "Grapes");
+        myList.add(1, "Papaya");
+        
+        System.out.println("Print All the Objects:");
+        for(String s:myList){
+            System.out.println(s);
+        }
+        
+        System.out.println("3rd element in the list is: " + myList.get(2));
+        System.out.println("Is Chicku is in list: " + myList.contains("Chicku"));
+        System.out.println("Size of ArrayList: " + myList.size());
+        
+        myList.remove("Papaya");
+        
+        System.out.println("New Size of ArrayList: " + myList.size());
+    }
 }
